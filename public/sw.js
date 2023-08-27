4c273dd7fe3fa16f6100e6eb4ebd86259cd27bd8
LDG-BBgod <padzz321@naver.com>
2023-08-28 01:01:35 +0900

    test

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,23 @@
 const self = this
 
 self.addEventListener('fetch', (event) => {
-  const requestUrl = new URL(event.request.url)
+  // Intercept the request and modify start_url if it's a navigation request
+  if (event.request.mode === 'navigate') {
+    const url = new URL(event.request.url);
+    const params = url.searchParams.toString(); // Get the query parameters
 
-  if (requestUrl.pathname.startsWith('/dynamic')) {
-    event.respondWith(handleDynamicRequest(event.request))
+    // Modify the start_url based on the current URL and its query parameters
+    const newStartUrl = `/mo?${params}`;
+    const manifestUrl = new URL('/manifest.json', self.location).toString();
+    
+    // Update the manifest's start_url
+    if (event.request.url === manifestUrl) {
+      const modifiedManifest = JSON.parse(event.request.headers.get('Manifest-Modified'));
+      modifiedManifest.start_url = newStartUrl;
+      const modifiedResponse = new Response(JSON.stringify(modifiedManifest), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      event.respondWith(modifiedResponse);
+    }
   }
-})
-
-async function handleDynamicRequest(request) {
-  const startUrl = getDynamicStartUrl(request)
-  const response = await fetch(startUrl)
-  return response
-}
-
-function getDynamicStartUrl(request) {
-  const params = new URLSearchParams(request.url.split('?')[1])
-  const url = params.get('url')
-  return url
-}
+});
