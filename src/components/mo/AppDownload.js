dd0c6879979f4cf816c8176602a15a573ab45258
LDG-BBgod <padzz321@naver.com>
2023-08-29 01:16:24 +0900

    test

--- src/components/mo/AppDownload.js
+++ src/components/mo/AppDownload.js
@@ -4,7 +4,7 @@ import Spacer from './Spacer'
 
 const AppDownload = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null)
-
+  alert(deferredPrompt)
   useEffect(() => {
     window.addEventListener('beforeinstallprompt', (event) => {
       event.preventDefault()
@@ -12,7 +12,56 @@ const AppDownload = () => {
     })
   }, [])
 
-  const installApp = () => {
+  const handleButton = () => {
+    if (window.navigator && window.navigator['standalone']) {
+      alert('이미 바탕화면에 추가되어 있습니다.')
+    } else if (window.matchMedia('(display-mode: standalone)').matches) {
+      alert('이미 바탕화면에 추가되어 있습니다.')
+    } else {
+      if (deferredPrompt) {
+        deferredPrompt.prompt()
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt')
+          } else {
+            console.log('User dismissed the A2HS prompt')
+          }
+        })
+      }
+    }
+  }
+
+  return (
+    <Box onClick={handleButton}>
+      <img src="/img/buttonLogo.svg" alt="logo" width={20} height={20} />
+      <Spacer horizontal={false} space={10} />
+      <Text>앱 다운로드</Text>
+      <Spacer horizontal={false} space={5} />
+      <img src="/img/download.svg" alt="down" width={9.5} height={11.5} />
+    </Box>
+  )
+}
+
+export default AppDownload
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  width: 145px;
+  height: 34px;
+  border-radius: 5px;
+  background-color: #5cdaa7;
+`
+
+const Text = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+`
+
+
     // const dynamicManifest = {
     //   name: 'CABO',
     //   short_name: 'CABO',
@@ -46,56 +95,3 @@ const AppDownload = () => {
     // link.rel = 'manifest'
     // link.href = manifestURL
     // document.head.appendChild(link)
-
-    if (deferredPrompt) {
-      deferredPrompt.prompt()
-      // deferredPrompt.userChoice.then((choiceResult) => {
-      //   if (choiceResult.outcome === 'accepted') {
-      //     console.log('User accepted the A2HS prompt')
-      //   } else {
-      //     console.log('User dismissed the A2HS prompt')
-      //   }
-      // })
-    }
-  }
-
-  const handleButton = () => {
-    alert('바탕화면에 바로가기를 추가합니다.')
-    // if (window.navigator && window.navigator['standalone']) {
-    //   alert('이미 바탕화면에 추가되어 있습니다.')
-    // } else if (window.matchMedia('(display-mode: standalone)').matches) {
-    //   alert('이미 바탕화면에 추가되어 있습니다.')
-    // } else {
-    installApp()
-    // }
-  }
-
-  return (
-    <Box onClick={handleButton}>
-      <img src="/img/buttonLogo.svg" alt="logo" width={20} height={20} />
-      <Spacer horizontal={false} space={10} />
-      <Text>앱 다운로드</Text>
-      <Spacer horizontal={false} space={5} />
-      <img src="/img/download.svg" alt="down" width={9.5} height={11.5} />
-    </Box>
-  )
-}
-
-export default AppDownload
-
-const Box = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  width: 145px;
-  height: 34px;
-  border-radius: 5px;
-  background-color: #5cdaa7;
-`
-
-const Text = styled.div`
-  font-size: 14px;
-  font-weight: bold;
-  color: #fff;
-`
